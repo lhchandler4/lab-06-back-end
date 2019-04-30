6d54dc268dcf6446da65638e18db8c69bb835d6b
williams osunkwo <wosunkwo@gmail.com>
2019-04-30 12:37:36 -0700

    finished the rest of the features

--- server.js
+++ server.js
@@ -2,43 +2,60 @@
 
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
+app.use(cors());
 
 const PORT = process.env.PORT || 3000;
-
-// app.use(express.static('./public'));
-
-app.get('/lab6', (request, response)=>{
-  response.status(200).send('Hey it works');
-});
+var weatherArr = [];
 
 
 app.get('/location', (request, response) => {
-  // try {
-  const locationData = require('./data/geo.json');
-  let searchQuery = request.query.data;
-  let formattedAddress = locationData.results[0].formatted_address;
-  let latitude = locationData.results[0].geometry.location.lat;
-  let longitude = locationData.results[0].geometry.location.lng;
+  try {
+    const locationData = require('./data/geo.json');
+    let searchQuery = request.query.data;
+    let formattedAddress = locationData.results[0].formatted_address;
+    let latitude = locationData.results[0].geometry.location.lat;
+    let longitude = locationData.results[0].geometry.location.lng;
 
-  let locationInstance = new Place(searchQuery, formattedAddress, latitude, longitude);
-  console.log(locationInstance);
-  response.status(200).send(locationInstance);
- 
- 
- 
-  // } catch( error ) {
-  //   console.log('Sorry, There was an Error');
-  //   response.status(500).send('Sorry, There was an Error');
-  // }
+    let locationInstance = new Place(searchQuery, formattedAddress, latitude, longitude);
+    // response.status(200).send(locationInstance);
+    response.send(locationInstance);
+  } catch( error ) {
+    console.log('Sorry, There was an Error');
+    response.status(500).send('Sorry, There was an Error');
+  }
 });
+
+
+app.get('/weather', (request, response) => {
+  try {
+    const weatherData = require('./data/darksky.json');
+    for(let i=0; i<8; i++){
+      let forecast =  weatherData.daily.data[i].summary;
+      let time = weatherData.daily.data[0].time;
+      new Weather(forecast, time);
+    }
+    response.status(200).send(weatherArr);
+
+  } catch( error ) {
+    console.log('Sorry, There was an Error');
+    response.status(500).send('Sorry, There was an Error');
+  }
+});
+
 
 app.listen(PORT,()=> console.log(`Listening on port ${PORT}`));
 
 function Place (searchQuery, formattedAddress, lat, lng) {
-  this.searchQuery = searchQuery;
-  this.formattedAddress = formattedAddress;
-  this.lat = lat;
-  this.lng = lng;
+  this.search_query = searchQuery;
+  this.formatted_query = formattedAddress;
+  this.latitude = lat;
+  this.longitude = lng;
 }
 
+function Weather (forecast, time) {
+  this.forecast = forecast;
+  this.time = new Date(time*1000).toDateString();
+  weatherArr.push(this);
+}
