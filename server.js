18dd4cd210f5ed06b2fb3c4a705d8cf76213c098
williams osunkwo <wosunkwo@gmail.com>
2019-04-30 11:41:00 -0700

    completed the server.js feauture1

--- server.js
+++ server.js
@@ -6,29 +6,39 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('./public'));
+// app.use(express.static('./public'));
 
 app.get('/lab6', (request, response)=>{
   response.status(200).send('Hey it works');
 });
 
-app.listen(PORT,()=> console.log(`Listening on port ${PORT}`));
 
 app.get('/location', (request, response) => {
-  try {
-    let locationData = require('./data/geo.json');
-    response.send(locationData.results[0].formatted_address);
-  } catch( error ) {
-    console.log('Sorry, There was an Error');
-    response.status(500).send('Sorry, There was an Error');
-  }
+  // try {
+  const locationData = require('./data/geo.json');
+  let searchQuery = request.query.data;
+  let formattedAddress = locationData.results[0].formatted_address;
+  let latitude = locationData.results[0].geometry.location.lat;
+  let longitude = locationData.results[0].geometry.location.lng;
+
+  let locationInstance = new Place(searchQuery, formattedAddress, latitude, longitude);
+  console.log(locationInstance);
+  response.status(200).send(locationInstance);
+ 
+ 
+ 
+  // } catch( error ) {
+  //   console.log('Sorry, There was an Error');
+  //   response.status(500).send('Sorry, There was an Error');
+  // }
 });
 
-function Place (formattedAddress, lat, lng) {
+app.listen(PORT,()=> console.log(`Listening on port ${PORT}`));
+
+function Place (searchQuery, formattedAddress, lat, lng) {
+  this.searchQuery = searchQuery;
   this.formattedAddress = formattedAddress;
   this.lat = lat;
   this.lng = lng;
 }
-
-var myLocation = new Place(' ',' ',' ');
 
